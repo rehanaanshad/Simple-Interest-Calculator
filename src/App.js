0319793f920e333663cbe43c2d752296ccec72bc
Rehana <rehanaanshad@gmail.com>
2023-10-12 22:06:38 +0530

    Simple-interest calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+
 import './App.css';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { useState } from 'react';
 
 function App() {
+  const [amount,setAmount]=useState(0)
+  const [rate,setRate] = useState(0)
+  const [year,setYear] = useState(0)
+  const [interest,setInterest] = useState(0)
+ 
+  const calculate=(e)=>{
+    const output =amount*year*rate/100
+    console.log(output);
+    setInterest(output)
+  }
+ 
+  const reset=(e)=>{
+    setAmount(0)
+    setYear(0)
+    setRate(0)
+    setInterest(0)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="container">
+          <div className="headings">
+            <h1>Simple Interest Calculator</h1>
+            <p>Calculate your simple interest with us </p>
+          </div>
+
+          <div className="total">
+          <h3>&#8377; {interest}</h3>
+          <p>Your Total Interest</p>
+          </div>
+          <div className='form'>
+          <form>
+            <div className='input'>
+          <TextField id="outlined-basic" label="Amount" 
+          onChange={(e)=>setAmount(e.target.value)}
+          value={amount||""}
+          variant="outlined" />
+
+          <TextField id="outlined-basic" label="Rate" 
+          onChange={(e)=>setRate(e.target.value)}
+          value={rate||""}
+          variant="outlined" />
+
+          <TextField id="outlined-basic" label="year"
+          onChange={(e)=>setYear(e.target.value)} 
+          value={year||""}
+          variant="outlined" />
+          </div>
+
+          <div className='button'>
+          <Button onClick={calculate} 
+          variant="contained">Calculate</Button>
+          <Button onClick={reset} variant="outlined">Reset</Button>
+
+          </div>
+          </form>
+          </div>
+        </div>
+        
     </div>
   );
 }
